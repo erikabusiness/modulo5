695d9a35a07fa22047620fd5c217075e271ef7de
Erika Alves <erikaalves.m.silva@gmail.com>
2023-12-16 01:59:42 -0300

    Uusuario e Destino OK

--- src/main/java/tde/modulo5/controller/ReservaController.java
+++ src/main/java/tde/modulo5/controller/ReservaController.java
@@ -1,20 +1,15 @@
 package tde.modulo5.controller;
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import tde.modulo5.entities.Destino;
 import tde.modulo5.entities.Reserva;
-import tde.modulo5.entities.Usuario;
 import tde.modulo5.repositories.DestinoRepository;
 import tde.modulo5.repositories.ReservaRepository;
 import tde.modulo5.repositories.UsuarioRepository;
@@ -38,7 +33,7 @@ public class ReservaController {
 
 	@GetMapping
 	public ModelAndView reservas() {
-		ModelAndView modelAndView = new ModelAndView("/reserva/index.html");
+		ModelAndView modelAndView = new ModelAndView("reserva/index.html");
 		
 		modelAndView.addObject("listaUsuarios", usuarioRepository.findAll());
 		modelAndView.addObject("listaDestino", destinoRepository.findAll());
@@ -52,25 +47,18 @@ public class ReservaController {
 	@GetMapping("/cadastrar")
     public ModelAndView cadastrar() {
         ModelAndView modelAndView = new ModelAndView("reserva/create"); //nome da pasta/ nome do html
-
         
-       /* modelAndView.addObject("usuarios", usuarios);*/
-       /* modelAndView.addObject("destinos", destinos);*/
-        modelAndView.addObject("reseva", new Reserva());
+        modelAndView.addObject("reserva", new Reserva());
+        modelAndView.addObject("listaUsuarios", usuarioRepository.findAll());
+		modelAndView.addObject("listaDestino", destinoRepository.findAll());
 
         return modelAndView;
     }
 
 	@PostMapping("/cadastrar")
-	public ModelAndView cadastrar(Reserva reserva, @RequestParam("usuario") Long usuario, @RequestParam("destino") Long destino) throws IOException {
-		ModelAndView modelAndView = new ModelAndView("redirect:/reserva");
+	public ModelAndView cadastrar(Reserva reserva) {
 		
-		Usuario usuario1 = new Usuario();
-		/*usuario1 = usuarioRepository.findById(usuario).orElse(null);*/
-		/*reserva.setUsuario(usuario1);*/
-		Destino destino1 = new Destino();
-		/*destino1 = destinoRepository.findById(destino).orElse(null);
-		reserva.setDestino(destino1);*/
+		ModelAndView modelAndView = new ModelAndView("redirect:/reserva");
 				
 		reservaRepository.save(reserva);
 		
@@ -81,11 +69,10 @@ public class ReservaController {
 	@GetMapping("/{id}/editar")
     public ModelAndView editar(@PathVariable Long id) {
         ModelAndView modelAndView = new ModelAndView("reserva/edit");
-        Reserva reserva= reservaRepository.getReferenceById(id);
+        modelAndView.addObject(reservaRepository.getReferenceById(id));
         
-        /*modelAndView.addObject("usuarios", usuarios);*/
-       /* modelAndView.addObject("destinos", destinos);*/
-        modelAndView.addObject("reserva", reserva);
+        modelAndView.addObject("listaUsuarios", usuarioRepository.findAll());
+		modelAndView.addObject("listaDestino", destinoRepository.findAll());
 
         return modelAndView;
     }
@@ -99,9 +86,11 @@ public class ReservaController {
     }
 
 	@GetMapping("/{id}/excluir")
-	public String excluir(@PathVariable Long id) {
-		reservaRepository.deleteById(id);
+    public ModelAndView excluir(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/reserva");
 
-		return "redirect:/reserva";
-	}
+        reservaRepository.deleteById(id);
+
+        return modelAndView;
+    }
 }
